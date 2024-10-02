c053bc39c6e5b96acaf8178bbb5b2e4d3492fd74
Tomás Cueto <tomasco98@gmail.com>
2024-10-02 20:44:31 -0300

    Commit inicial

---
+++ hernans-webpage/app/layout.tsx
@@ -0,0 +1,18 @@
+import {lato} from '@/app/ui/fonts'
+import "./ui/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${lato.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ hernans-webpage/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lato } from 'next/font/google';
+ 
+export const lato = Lato({ 
+  weight: ['400', '700'], 
+  subsets: ['latin'] });
